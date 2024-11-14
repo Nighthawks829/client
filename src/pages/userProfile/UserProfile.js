643790e4ae9604aaeafc50ca63f6dc07492c2199
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-11-14 13:37:12 +0800

    Modified user profile to get the user detail with the auth store

--- src/pages/userProfile/UserProfile.js
+++ src/pages/userProfile/UserProfile.js
@@ -1,59 +1,14 @@
 import React from "react";
 import "./UserProfile.css";
-import { IoMdArrowDropdown } from "react-icons/io";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 
 function UserProfile() {
-  const navigate=useNavigate()
+  const { user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+
   return (
     <>
-      {/* <nav
-        className="navbar navbar-expand-sm bg-dark border-bottom border-body p-3"
-        data-bs-theme="dark"
-      >
-        <div className="container-fluid px-4">
-          <a className="navbar-brand" href="/homePage">
-            To Do Website
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <div className="ms-auto d-flex align-items-center">
-              <img
-                src={require("../../assets/profile.jpg")}
-                alt="profile-picture"
-                className="profile-picture rounded-circle"
-              />
-              <p className="profile-name text-light mb-0 ms-3 me-1">
-                Sunlightsam
-              </p>
-              <div className="dropdown">
-                <IoMdArrowDropdown
-                  color="white"
-                  className="dropdown-toggle"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                />
-                <ul className="dropdown-menu dropdown-menu-end mt-3 py-3">
-                  <li>
-                    <a className="dropdown-item">User Profile</a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item text-danger">Logut</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav> */}
       <div className="container my-5">
         <div className="row g-0">
           <div className="col-md-6 custom-height">
@@ -70,21 +25,22 @@ function UserProfile() {
               <div>
                 <h1 className="userprofile-title mb-5">User Profile</h1>
                 <h3 className="mb-3">
-                  Name: <span className="information-text">Nighthawks</span>
+                  Name: <span className="information-text">{user.name}</span>
                 </h3>
                 <h3 className="mb-3">
-                  Email:{" "}
-                  <span className="information-text">
-                    nighthawks0230@gmail.com
-                  </span>
+                  Email: <span className="information-text">{user.email}</span>
                 </h3>
-                <h3 className="mb-3">
-                  Created At:{" "}
-                  <span className="information-text">10 JAN 2012</span>
-                </h3>
-              <div className="mt-5 text-md-start text-center">
-                <button className="btn btn-success shadow col-md-5 col-5 fs-4" onClick={()=>navigate("/edituserprofile")}>Edit Profile</button>
-              </div>
+                {/* <h3 className="mb-3">
+                  Created At: <span className="information-text">{user.createdAt}</span>
+                </h3> */}
+                <div className="mt-5 text-md-start text-center">
+                  <button
+                    className="btn btn-success shadow col-md-6 col-5 fs-4"
+                    onClick={() => navigate("/edituserprofile")}
+                  >
+                    Edit Profile
+                  </button>
+                </div>
               </div>
             </div>
           </div>
